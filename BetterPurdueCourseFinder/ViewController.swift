b76c67552f7aec8c48d51b70221fc457b4bc49c5
Kedar Abhyankar <krabhyankar@gmail.com>
2021-01-13 12:44:32 -0500

    Initial Commit

---
+++ BetterPurdueCourseFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BetterPurdueCourseFinder
+//
+//  Created by Kedar Abhyankar on 1/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
